7ee14eb73c4601ae250e386fe952b3959f1e94de
Brindha-D18 <brindha200518@gmail.com>
2025-12-19 06:43:00 +0530

    Update OpenRouterClient.java

--- EduQuiz/src/main/java/com/aiquiz/util/OpenRouterClient.java
+++ EduQuiz/src/main/java/com/aiquiz/util/OpenRouterClient.java
@@ -1,6 +1,8 @@
 package com.aiquiz.util;
 public class OpenRouterClient {
 public static String generateQuestions(String topic,String level){
-return "AI Generated Questions JSON (Mock)";
+return " Generate 10 multiple-choice questions on JAVA with HARD difficulty.
+Each question must have 4 options and one correct answer.
+Return output in JSON format.";
 }
 }
